7ce3190c8403f524708991fc282801d05f64dbfe
Opstober <78286451+Opstober@users.noreply.github.com>
2022-08-12 22:35:37 +0700

    Update simsimi.py

--- simsimi.py
+++ simsimi.py
@@ -12,6 +12,6 @@ def simsimi(content):
     url = f'https://api.simsimi.net/v2/?text={content}&lc={language}'
     response = scraper.get(url).text
     soup = BeautifulSoup(response, 'html.parser')
-    print("Respond: " + str(soup))
+    # print("Respond: " + str(soup))
     text = json.loads(soup.text)
     return text["success"]
